711e7fe68148f509bdd676471f7df219ce10bc59
Myron <dubtwee@gmail.com>
2020-10-09 12:57:59 -0500

    collecting the inputted values for a new pokemon

--- scripts/client.js
+++ scripts/client.js
@@ -4,4 +4,18 @@ $(document).ready(onReady);
 
 function onReady() {
   console.log('Stuff is ready');
+
+  $('.js-click-add').on('click', clickToAdd);
+}
+
+function clickToAdd() {
+  // capture input info
+  const name = $('.js-field-name').val();
+  const hp = $('.js-field-hp').val();
+  const att = $('.js-field-att').val();
+  const def = $('.js-field-def').val();
+  const image = $('.js-field-image').val();
+  console.log(name, hp, att, def, image);
+
+  // store pokemon data
 }
